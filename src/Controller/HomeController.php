1592eed1415cbbf03c6732b735d86156131fb9df
Raphael Moynat <raphael.moynat@gmail.com>
2025-03-04 10:15:50 +0100

    php mailer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 use Core\Attributes\Route;
-
+use Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 
 
 class HomeController extends \Core\Controller\Controller
@@ -10,6 +11,7 @@ class HomeController extends \Core\Controller\Controller
     #[Route(path: "/", name: "app_home")]
     public function index()
     {
+
         echo "page home";
 
 
@@ -24,5 +26,25 @@ class HomeController extends \Core\Controller\Controller
 
 
     }
+
+  #[Route(path: "/mail", name: "app_mail")]
+  public function dockerMailer() {
+    $mail = new PHPMailer(true);
+
+    try {
+      $mail->isSMTP();
+      $mail->Host = 'localhost';
+      $mail->Port = 1025;
+      $mail->SMTPAuth   = false;
+      $mail->isHTML(true);
+      $mail->setFrom('test@mail.com', 'test');
+      $mail->addAddress('luc@mail.com','luc' );
+      $mail->Body = "test coucou";
+      $mail->send();
+      echo 'Message has been sent';
+    } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+  }
 
 }
